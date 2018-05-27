d47787d3605868900f1c18dccfc6f0c0126442cb
okyisother <oky.octaviansyah@yahoo.co.id>
2018-05-27 22:02:48 +0700

    master

---
+++ system/language/english/form_validation_lang.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under Isian MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (Isian "Software"), to deal
+ * in Isian Software without restriction, including without limitation Isian rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of Isian Software, and to permit persons to whom Isian Software is
+ * furnished to do so, subject to Isian following conditions:
+ *
+ * Isian above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of Isian Software.
+ *
+ * Isian SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO Isian WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL Isian
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OIsianR
+ * LIABILITY, WHEIsianR IN AN ACTION OF CONTRACT, TORT OR OIsianRWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH Isian SOFTWARE OR Isian USE OR OIsianR DEALINGS IN
+ * Isian SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= 'Isian {field} dibutuhkan.';
+$lang['form_validation_isset']			= 'Isian {field} tidak boleh kosong.';
+$lang['form_validation_valid_email']		= 'Isian {field} harus mengandung format emai yang sesuai.';
+$lang['form_validation_valid_emails']		= 'Semua Isian {field} harus mengandung format emai yang sesuai.';
+$lang['form_validation_valid_url']		= 'Isian {field} harus mengandung format uri yang sesuai.';
+$lang['form_validation_valid_ip']		= 'Isian {field} harus mengandung format IP yang sesuai.';
+$lang['form_validation_min_length']		= 'Isian {field} panjangnya harus melebihi {param} karakter.';
+$lang['form_validation_max_length']		= 'Isian {field} panjangnya tidak boleh melebihi {param} karakter.';
+$lang['form_validation_exact_length']		= 'Isian {field} field must be exactly {param} characters in length.';
+$lang['form_validation_alpha']			= 'Isian {field} field may only contain alphabetical characters.';
+$lang['form_validation_alpha_numeric']		= 'Isian {field} field may only contain alpha-numeric characters.';
+$lang['form_validation_alpha_numeric_spaces']	= 'Isian {field} field may only contain alpha-numeric characters and spaces.';
+$lang['form_validation_alpha_dash']		= 'Isian {field} field may only contain alpha-numeric characters, underscores, and dashes.';
+$lang['form_validation_numeric']		= 'Isian {field} hanya boleh diisi dengan angka .';
+$lang['form_validation_is_numeric']		= 'Isian {field} hanya boleh diisi dengan karakter numeric.';
+$lang['form_validation_integer']		= 'Isian {field} field must contain an integer.';
+$lang['form_validation_regex_match']		= 'Isian {field} field is not in Isian correct format.';
+$lang['form_validation_matches']		= 'Isian {field} field does not match Isian {param} field.';
+$lang['form_validation_differs']		= 'Isian {field} field must differ from Isian {param} field.';
+$lang['form_validation_is_unique'] 		= 'Isian {field} harus diisikan email yang unik.';
+$lang['form_validation_is_natural']		= 'Isian {field} field must only contain digits.';
+$lang['form_validation_is_natural_no_zero']	= 'Isian {field} field must only contain digits and must be greater than zero.';
+$lang['form_validation_decimal']		= 'Isian {field} field must contain a decimal number.';
+$lang['form_validation_less_than']		= 'Isian {field} field must contain a number less than {param}.';
+$lang['form_validation_less_than_equal_to']	= 'Isian {field} field must contain a number less than or equal to {param}.';
+$lang['form_validation_greater_than']		= 'Isian {field} field must contain a number greater than {param}.';
+$lang['form_validation_greater_than_equal_to']	= 'Isian {field} field must contain a number greater than or equal to {param}.';
+$lang['form_validation_error_message_not_set']	= 'Unable to access an error message corresponding to your field name {field}.';
+$lang['form_validation_in_list']		= 'Isian {field} field must be one of: {param}.';
